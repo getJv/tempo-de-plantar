a64682e5f7a39f65caab2e9e22764d1ef4fe45af
Jhonatan Morais <jhonatan.morais@gmail.com>
2020-11-27 23:43:53 -0300

    first heroku publish

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCgTL4UUd0_MGgp4sB6hUb2ajzIEbLE1YQ",
-    authDomain: "general-api-d2532.firebaseapp.com",
-    databaseURL: "https://general-api-d2532.firebaseio.com",
-    projectId: "general-api-d2532",
-    storageBucket: "general-api-d2532.appspot.com",
-    messagingSenderId: "198831312639",
-    appId: "1:198831312639:web:e335de54563205bf5846ca"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
